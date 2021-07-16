2c42ba51c3f0383a89885e630890cedc9c1da92d
takurinton <takurinton@takurinton.com>
2021-07-16 17:23:15 +0900

    add: 80$

--- body.js
+++ body.js
@@ -1,13 +1,16 @@
+const scroll = 0.8;
 const _options = {
     root: null,
     rootMargin: '-100% 0px 1000% 0px',
-    threshold: [.2],
+    threshold: 1 - scroll,
 }
 
-const _observer = new IntersectionObserver((targets) => {
+const _observer = new IntersectionObserver(targets => {
   for (const target of targets) {
-      if (target.isIntersecting) {
-        console.log('here')
+      if (!target.isIntersecting) {
+        console.log('here is 80%');
+      } else {
+          console.log('out of viewport');
       }
   };
 }, _options);
